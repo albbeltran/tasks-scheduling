6a1d3f0f889c268599b459729ca59881380a9b73
Alberto Beltran <albertobeltran@protonmail.com>
2025-06-08 16:10:41 -0600

    Enhanced consumer task queue function

--- src/sub_task_queue.ts
+++ src/sub_task_queue.ts
@@ -1,29 +1,40 @@
 import amqp from "amqplib/callback_api.js";
 
-export default async function startConsumer() {
-    amqp.connect(process.env.AMQP_URL!, function(connErr, connection) {
-      if (connErr) throw connErr;
+export default function startConsumer() {
+    amqp.connect(process.env.AMQP_URL!, (connErr, connection) => {
+        if (connErr) throw connErr;
 
-      connection.createChannel(function(channErr, channel) {
-        if(channErr) throw channErr;
-        channel.on("error", function(err) {
-            console.error("[AMQP] channel error", err.message);
+        connection.on("error", err => {
+            console.error("[AMQP] connection error", err.message);
         });
-        channel.on("close", function() {
-            console.log("[AMQP] channel closed");
+        connection.on("close", () => {
+            console.log("[AMQP] connection closed");
         });
-        const queue = 'tasks';
-    
-        channel.assertQueue(queue, {
-          durable: false
+
+        connection.createChannel((channErr, channel) => {
+            if (channErr) throw channErr;
+            channel.on("error", err => {
+                console.error("[AMQP] channel error", err.message);
+            });
+            channel.on("close", () => {
+                console.log("[AMQP] channel closed");
+            });
+            const queue = 'tasks';
+
+            channel.assertQueue(queue, {
+                durable: false
+            });
+
+            console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+            channel.consume(queue, msg => {
+                console.log(" [x] Received %s", msg?.content);
+                setTimeout(() => {
+                    console.log(" [x] Done");
+                    channel.ack(msg!);
+                }, 1000);
+            }, {
+                noAck: false
+            });
         });
-  
-        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
-        channel.consume(queue, function(msg) {
-          console.log(" [x] Received %s", msg?.content);
-        }, {
-            noAck: true
-        });
-      });
     });
-  }
+}
